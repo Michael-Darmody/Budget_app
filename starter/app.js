b9e404ba3f0a17758204d8509f0566c0c24269dd
Michael <darmodymichael@gmail.com>
2019-09-30 13:12:38 +0100

    UI now updates when an item is added

--- starter/app.js
+++ starter/app.js
@@ -57,7 +57,9 @@ var UIController = (function() {
     inputType: '.add__type',
     inputDescription: '.add__description',
     inputValue: '.add__value',
-    addBtn: '.add__btn'
+    addBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expenseContainer: '.expenses__list',
   };
 
   return {
@@ -67,6 +69,27 @@ var UIController = (function() {
          description: document.querySelector(DOMStrings.inputDescription).value,
          value: document.querySelector(DOMStrings.inputValue).value
       }
+    },
+
+    addListItem: function(obj, type) {
+      var html, newHtml, element;
+      // Create HTML string with placeholder text
+
+      if(type === 'inc') {
+        element = DOMStrings.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if(type === 'exp') {
+        element = DOMStrings.expenseContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      // Replace the placeholder text with some actual data
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+      // Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
     getDOMStrings: function() {
@@ -99,7 +122,8 @@ var  appController = (function(budgetCtrl, UICtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the item to the UI
-
+    UICtrl.addListItem(newItem, input.type)
+    
     // 4. Calculate the budget
 
     // 5. Display the budget on the UI
