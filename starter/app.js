e374c8b05839a2b9731525b426d70bd30be59b10
Michael <darmodymichael@gmail.com>
2019-10-08 18:55:55 +0100

    Added some finishing touches to the UI to improve UX

--- starter/app.js
+++ starter/app.js
@@ -166,6 +166,12 @@ var UIController = (function() {
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
   };
 
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return {
     getInput: function() {
       return {
@@ -218,12 +224,6 @@ var UIController = (function() {
     displayPercentages: function(percentages) {
       var fields = document.querySelectorAll(DOMStrings.expensePercLable);
 
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      }
-
       nodeListForEach(fields, function(current, index) {
         if(percentages[index] > 0) {
           current.textContent = percentages[index] + '%';
@@ -260,6 +260,16 @@ var UIController = (function() {
       document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' '+ year;
     },
 
+    changeType: function() {
+      var fields = document.querySelectorAll(DOMStrings.inputType + ',' + DOMStrings.inputDescription + ',' + DOMStrings.inputValue);
+
+      nodeListForEach(fields, function(cur) {
+        cur.classList.toggle('red-focus');
+      });
+
+      document.querySelector(DOMStrings.addBtn).classList.toggle('red');
+    },
+
     getDOMStrings: function() {
       return DOMStrings
     }
@@ -280,6 +290,8 @@ var  appController = (function(budgetCtrl, UICtrl) {
         ctrlAddItem();
       }
     });
+
+    document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changeType);
   };
 
   var updateBudget = function() {
